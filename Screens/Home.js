19531f970526e810d2a46475132c0481292d2d1c
Mohamed Ahmed <ma07041705@gmail.com>
2023-02-16 07:55:56 +0300

    first commit

---
+++ Screens/Home.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Home = () => {
+  return (
+    <View>
+      <Text className='items-center justify-center bg-red-600 '>Home</Text>
+    </View>
+  )
+}
+
+export default Home
